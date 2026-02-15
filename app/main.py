d54f8ff434b26822d773bbf66842a623c9618abc
Maksym Diadiushka <maks.d19@ukr.net>
2026-02-15 13:58:12 +0200

    corrected

--- app/main.py
+++ app/main.py
@@ -9,41 +9,43 @@ class Distance:
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __add__(self, other: int) -> "Distance":
+    def __add__(self, other: "Distance | int | float") -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         else:
             return Distance(self.km + other)
 
-    def __mul__(self, other: int) -> "Distance":
+    def __mul__(self, other: "Distance | int | float") -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
+        raise TypeError("Unsupported operand type for *")
 
-    def __truediv__(self, other: int) -> "Distance":
+    def __truediv__(self, other: "Distance | int | float") -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
+        raise TypeError("Unsupported operand type for /")
 
-    def __gt__(self, other: int) -> bool:
+    def __gt__(self, other: "Distance | int | float") -> bool:
         value = other.km if isinstance(other, Distance) else other
         return self.km > value
 
-    def __eq__(self, other: int) -> bool:
+    def __eq__(self, other: "Distance | int | float") -> bool:
         value = other.km if isinstance(other, Distance) else other
         return self.km == value
 
-    def __le__(self, other: int) -> bool:
+    def __le__(self, other: "Distance | int | float") -> bool:
         value = other.km if isinstance(other, Distance) else other
         return self.km <= value
 
-    def __ge__(self, other: int) -> bool:
+    def __ge__(self, other: "Distance | int | float") -> bool:
         value = other.km if isinstance(other, Distance) else other
         return self.km >= value
 
-    def __lt__(self, other: int) -> bool:
+    def __lt__(self, other: "Distance | int | float") -> bool:
         value = other.km if isinstance(other, Distance) else other
         return self.km < value
 
-    def __iadd__(self, other: int) -> "Distance":
+    def __iadd__(self, other: "Distance | int | float") -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         else:
